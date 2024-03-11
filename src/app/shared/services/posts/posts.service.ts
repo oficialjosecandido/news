26ab6fff60af62b116f0c840856c5cdb9ccc3842
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2024-03-11 20:59:11 +0000

    rest api working

--- src/app/shared/services/posts/posts.service.ts
+++ src/app/shared/services/posts/posts.service.ts
@@ -9,6 +9,11 @@ export class PostsService {
 
   constructor(private http: HttpClient) {}
 
+  getPosts()  {
+    console.log('getPosts');
+    return this.http.get<any[]>('https://tejomag.com/wp-json/wp/v2/posts')
+  }
+
   searchPosts(term: string): Observable<any[]> {
     // Replace 'your-api-endpoint' with the actual endpoint to fetch posts
     return this.http.get<any[]>(`your-api-endpoint?search=${term}`);
